f18379acf7893cb50810c3d3f665cfebe14e86e3
hugo alves <hugoalves958@gmail.com>
2026-02-24 22:13:25 -0300

    Aula 1 - Variáveis em JavaScript

---
+++ Aula1/src/script.js
@@ -0,0 +1,14 @@
+// Exemplo de variável let
+
+let nome ='Hugo';
+let nome2 = 'Davy';
+
+console.log(nome);
+console.log(nome2);
+
+// Exemplo de variável const
+
+const times = ['Palmeiras','Paysandu', 'Fortaleza'];
+
+console.log(times);
+
