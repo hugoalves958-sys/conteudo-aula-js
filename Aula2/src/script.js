5f25aae0b3b096182ccec54f03f84bcb21350cb6
hugo alves <hugoalves958@gmail.com>
2026-02-25 21:18:32 -0300

    Aula 2 - Continuação de Variáveis

---
+++ Aula2/src/script.js
@@ -0,0 +1,105 @@
+// Exemplo de operador de atribuição
+let x = 10;
+
+x = x - 5;
+//console.log(x);
+
+x = x + 5;
+//console.log(x);
+
+// Exemplos de Aritmética em JavaScript
+
+let soma = 20 + 5 + 2;
+console.log(soma);
+
+//const primeiroNome = 'Hugo';
+//const ultimoNome = 'Silva';
+
+let nomeCompleto = 'Hugo ' + 'Silva';
+
+//console.log(nomeCompleto);
+
+// Exemplos de tipos dem JavaScript
+//String
+
+let cor = 'Verde';
+let fruta = 'Limão';
+
+console.log(cor);
+console.log(fruta);
+
+//Number
+let soma1 = 10 + 10;
+let subtracao = 5 - 2;
+
+console.log(soma1);
+console.log(subtracao);
+
+// BigInt
+let bigint = 1234567890123456789012345n;
+
+let bigint2 = BigInt(1234567890123456789012345n);
+
+console.log(bigint);
+console.log(bigint2);
+
+// Bolean
+
+let valorVerdadeiro = true;
+let valorFalso = false;
+console.log(valorVerdadeiro);
+console.log(valorFalso);
+
+// Object 
+
+const caneta = {
+    marca: 'Bic',
+    modelo: 'esferográfica',
+    cor: 'Azul',
+}
+
+console.log(caneta);
+
+// Array Object
+
+const mochila = ['Estojo','Lapís','Régua','Caderno'];
+
+console.log(mochila);
+
+// Date Object
+
+const data = new Date;
+
+console.log(data);
+
+// Undefined
+let  indefinido;
+
+console.log(indefinido);
+
+// Null
+
+let nulo = null;
+console.log(nulo);
+
+// Symbol 
+
+let y = Symbol();
+console.log(y);
+
+// Exemplos de typeof 
+let stringTipo = '200';
+let numberTipo = 500;
+
+const tipoDado = typeof numberTipo;
+
+console.log(tipoDado);
+
+// Exemplo de Boolean
+
+let cinco = 5;
+let comparacao = cinco == 8;
+
+console.log(comparacao);
+
+
