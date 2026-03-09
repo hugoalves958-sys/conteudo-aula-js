6a9be0551f96f5a71b4ff41764561e94221d8813
hugo alves <hugoalves958@gmail.com>
2026-03-09 19:56:13 -0300

    Aula 8 - Métodos de Número em JavaScript

---
+++ Aula8/src/script.js
@@ -0,0 +1,30 @@
+// Utilização do método toString()
+let numero = 123;
+let extoString = numero.toString();
+console.log(typeof extoString);
+
+// Utilização do método toExponential()
+let a = 9.646;
+let exToExponential = a.toExponential(2);
+console.log(exToExponential);
+
+// Utilização do método toFixed()
+let b = 84.23048987;
+let exToFixed = b.toFixed(3);
+console.log(exToFixed);
+
+// Utilização do método to Precision()
+let c = 93.3932745923;
+let exToPrecision = c.toPrecision(6);
+console.log(exToPrecision);
+
+// Utilização do método valueOf()
+let d = 2004;
+let exValueOf = d.valueOf();
+console.log(exValueOf);
+
+// Utilização do método Number()
+let e = "593";
+let exNumber = Number(e);
+console.log(exNumber);
+
