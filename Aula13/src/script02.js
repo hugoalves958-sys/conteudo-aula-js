62067d8539fa6687eeed2e901d5b8e85d6a2c848
hugo alves <hugoalves958@gmail.com>
2026-03-13 22:10:15 -0300

    Aula 13 - Funções de Busca em Arrays parte 1

---
+++ Aula13/src/script02.js
@@ -0,0 +1,22 @@
+// Definindo uma array Global
+const times = ["Palmeiras","Flamengo", "Vasco","Bahia"];
+
+// Utilizando o método indexOf()
+let posicaoItem = times.indexOf("Vasco");
+
+console.log(times);
+console.log(posicaoItem);
+
+// Utilizando o método lastIndexOf()
+const time2 = ["Barcelona", "Real Madrid","Barcelona", "Barcelona"];
+
+let posicaoItem2 = time2.lastIndexOf("Barcelona");
+
+console.log(time2);
+console.log(posicaoItem2);
+
+// Utilizando o método includes()
+let verificaExistenciaItem = times.includes("Psg");
+
+console.log(verificaExistenciaItem);
+
