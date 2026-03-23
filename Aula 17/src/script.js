fb2bf12184cae18cd7fa5e53bdfcd3000994ce99
hugo alves <hugoalves958@gmail.com>
2026-03-23 19:26:54 -0300

    Aula 17 - Estudando eventos de carregamento

---
+++ Aula 17/src/script.js
@@ -0,0 +1,10 @@
+// Utilizando o evento de carregamento load
+
+const img = document.querySelector(".minha-imagem");
+const elemento = document.querySelector(".info-carregamento");
+
+img.addEventListener("load" , function() {
+    elemento.innerHTML = "A imagem foi carregada.";
+});
+
+
